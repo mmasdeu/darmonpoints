Fixed base points to reduce number of integrals. Relaxed sign condition whenever possible.

arithgroup.py
@@ -629,13 +629,15 @@ class ArithGroup_rationalquaternion(ArithGroup_generic):
         return V
 
     def _fix_sign(self,x,N):
+        verbose('Fixing sign...')
         emb = self.F.embeddings(RealField(100))[0]
         if emb(x.reduced_norm()).sign() != emb(N).sign():
             x = x * self.element_of_norm(-1,use_magma = False)
         assert emb(x.reduced_norm()).sign() == emb(N).sign()
+        verbose('Done fixing sign')
         return x
 
-    def element_of_norm(self,N,use_magma = False,return_all = False,radius = -1,max_elements = -1): # in rationalquaternion
+    def element_of_norm(self,N,use_magma = False,return_all = False,radius = -1,max_elements = -1,force_sign = True): # in rationalquaternion
         N = ZZ(N)
         if return_all == False:
             try:
@@ -650,7 +652,9 @@ class ArithGroup_rationalquaternion(ArithGroup_generic):
             elt_magma = self._O_magma.ElementOfNorm(N*self._F_magma.Integers())
             candidate = self.B([magma_F_elt_to_sage(self.F,elt_magma.Vector()[m+1]) for m in range(4)])
 
-            self._element_of_norm[N] = self._fix_sign(candidate,N)
+            if force_sign:
+                candidate = self._fix_sign(candidate,N)
+            self._element_of_norm[N] = candidate
             if return_all:
                 return [candidate]
             else:
@@ -713,7 +717,7 @@ class ArithGroup_rationalquaternion(ArithGroup_generic):
             return self._cache_hecke_reps[l]
         except KeyError: pass
         verbose('Finding hecke reps for l = %s'%l)
-        g0 = self.element_of_norm(l,use_magma = use_magma)
+        g0 = self.element_of_norm(l,use_magma = use_magma,force_sign = False)
         assert g0.reduced_norm() == l
         reps = [g0]
         ngens = len(self.gens())
@@ -823,7 +827,7 @@ class ArithGroup_rationalmatrix(ArithGroup_generic):
             tmp.extend(self.minus_one)
         return tmp
 
-    def element_of_norm(self,N,use_magma = False,local_condition = None): # in rationalmatrix
+    def element_of_norm(self,N,use_magma = False,local_condition = None,force_sign = True): # in rationalmatrix
         try:
             return self._element_of_norm[N]
         except (AttributeError,KeyError):
@@ -1223,12 +1227,15 @@ class ArithGroup_nf_quaternion(ArithGroup_generic):
         assert emb(x.reduced_norm()).sign() == emb(N).sign()
         return x
 
-    def element_of_norm(self,N,use_magma = False,return_all = False,radius = -1,max_elements = -1): # in nf_quaternion
+    def element_of_norm(self,N,use_magma = False,return_all = False,radius = -1,max_elements = -1,force_sign = True): # in nf_quaternion
         Nideal = self.F.ideal(N)
         if return_all == False:
             try:
                 if use_magma:
-                    return self._fix_sign(self._element_of_norm[Nideal.gens_two()],N)
+                    if force_sign:
+                        return self._fix_sign(self._element_of_norm[Nideal.gens_two()],N)
+                    else:
+                        return self._element_of_norm[Nideal.gens_two()]
                 else:
                     return self._element_of_norm[N]
             except (AttributeError,KeyError):
@@ -1246,10 +1253,12 @@ class ArithGroup_nf_quaternion(ArithGroup_generic):
             elt_magma_vector = elt_magma.Vector()
             candidate = self.B([magma_F_elt_to_sage(self.F,elt_magma_vector[m+1]) for m in range(4)])
             self._element_of_norm[Nideal.gens_two()] = candidate
+            if force_sign:
+                candidate = self._fix_sign(candidate,N)
             if return_all:
-                return [self._fix_sign(candidate,N)]
+                return [candidate]
             else:
-                return self._fix_sign(candidate,N)
+                return candidate
         else:
             v = self.Obasis
             verbose('Doing long enumeration...')

sarithgroup.py
@@ -302,7 +302,6 @@ class BigArithGroup_class(AlgebraicGroup):
                 newEgood.extend([BTEdge(not rev, e * gamma) for e in self.get_BT_reps()[1:]])
         return self.subdivide(newEgood,1-parity,depth - 1)
 
-    #@lazy_attribute
     @cached_method
     def wp(self):
         verbose('Finding a suitable wp...')
@@ -312,12 +311,10 @@ class BigArithGroup_class(AlgebraicGroup):
             epsinv = matrix(QQ,2,2,[0,-1,self.p,0])**-1
             pfacts = self.F.maximal_order().ideal(self.p).factor() if self.F.degree() > 1 else ZZ(self.p).factor()
 
-            try:
-                all_elts = self.Gn.element_of_norm(self.ideal_p.gens_reduced()[0],use_magma = True,return_all = True,max_elements = 1) #,radius = 5, max_elements = -1)
-                # all_elts = self.Gpn.element_of_norm(self.ideal_p.gens_reduced()[0],use_magma = False,return_all = True,radius = 5, max_elements = -1)
-            except AttributeError:
-                # all_elts = self.Gn.element_of_norm(self.p,use_magma = True,return_all = True,max_elements = 1) #,radius = 5, max_elements = -1)
-                all_elts = self.Gpn.element_of_norm(self.ideal_p,use_magma = False,return_all = True,radius = 5,max_elements = 1)
+            if self.F == QQ:
+                all_elts = self.Gpn.element_of_norm(self.ideal_p,use_magma = False,return_all = True,radius = 5,max_elements = 1,force_sign = False)
+            else:
+                all_elts = self.Gn.element_of_norm(self.ideal_p.gens_reduced()[0],use_magma = True,return_all = True,max_elements = 1,force_sign = False)
 
             found = False
             all_initial = all_elts