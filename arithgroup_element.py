57fa24955e40f10f8714818c1bfa77b0ac286469
Marc Masdeu <marc.masdeu@gmail.com>
2014-07-10 14:50:56 +0100

    Cleaned up some code.

--- arithgroup_element.py
+++ arithgroup_element.py
@@ -181,26 +181,7 @@ class ArithGroupElement(MultiplicativeGroupElement):
         #return self.parent().get_weight_vector(self) in self.parent().get_relation_matrix().image()
         return self.parent().abelianization()(self) == 0
 
-    def find_bounding_cycle(self,G,method = 'original',npow = 1,check = False):
-        if method == 'original':
-            ans = self.find_bounding_cycle_original(G,npow = npow)
-        elif method == 'short':
-            ans = self.find_bounding_cycle_short(G,npow = npow)
-        else:
-            raise ValueError
-        if check:
-            # DEBUG
-            from homology import Homology
-            Coh = Homology(G.Gn.B,ZZ)
-            tmp = Coh(dict([(self.quaternion_rep**npow,ZZ(1))]))
-            for m,xlist,y in ans:
-                for x in xlist:
-                    tmp -= Coh(dict([(x,ZZ(m))])) + Coh(dict([(y.quaternion_rep,ZZ(m))])) - Coh(dict([(x*y.quaternion_rep,ZZ(m))]))
-            if tmp.size_of_support() !=0:
-                verbose('Leftover of %s'%tmp)
-        return ans
-
-    def find_bounding_cycle_short(self,G,npow = 1):
+    def find_bounding_cycle(self,G,npow = 1,check = False):
         r'''
         Use recursively that:
         - x^a g = a x + g - del(x^a|g) - del(x|(x + x^2 + ... + x^(a-1)))
@@ -249,6 +230,16 @@ class ArithGroupElement(MultiplicativeGroupElement):
                 ans.extend([(m,[gaq**-1],ga)] if a < 0 else [])
                 # add the boundaries of g^abs(a)
                 ans.extend([(-sgn(a)*m,[gq**j for j in range(1,abs(a))],g)] if abs(a) > 1 else [])
+        if check:
+            # DEBUG
+            from homology import Homology
+            Coh = Homology(G.Gn.B,ZZ)
+            tmp = Coh(dict([(self.quaternion_rep**npow,ZZ(1))]))
+            for m,xlist,y in ans:
+                for x in xlist:
+                    tmp -= Coh(dict([(x,ZZ(m))])) + Coh(dict([(y.quaternion_rep,ZZ(m))])) - Coh(dict([(x*y.quaternion_rep,ZZ(m))]))
+            if tmp.size_of_support() !=0:
+                verbose('Leftover of %s'%tmp)
         return ans
 
     def find_bounding_cycle_original(self,G,npow = 1):
