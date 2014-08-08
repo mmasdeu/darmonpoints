e6cefec2d818c26b8ba0de5d1bcdb3733a41037a
Marc Masdeu <marc.masdeu@gmail.com>
2014-08-08 19:02:07 +0100

    Fixed small bug.

--- cohomology.py
+++ cohomology.py
@@ -671,7 +671,7 @@ class CohomologyGroup(Parent):
                     num_hecke_operators += 1
                     old_component_list = component_list
                     component_list = []
-                    Aq = self.hecke_matrix(qq.gens_reduced()[0],g0 = g0)
+                    Aq = self.hecke_matrix(qq.gens_reduced()[0],g0 = g0).change_ring(QQ)
                     for U in old_component_list:
                         for U0,is_irred in Aq.decomposition_of_subspace(U):
                             if U0.dimension() == 1:
