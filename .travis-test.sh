6351bbcf184728947cb2e38a72001d8f78ab2f0c
Marc Masdeu <marc.masdeu@gmail.com>
2017-07-19 10:28:21 -0400

    Added index to pypi.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 set -e
-$HOME/SageMath/sage -pip install --upgrade --no-index -v .
+
+$HOME/SageMath/sage -pip install --upgrade --no-index -v -i https://pypi.python.org/pypi .
 $HOME/SageMath/sage setup.py test
 (cd docs && $HOME/SageMath/sage -sh -c "make html")
 $HOME/SageMath/sage -pip uninstall .
