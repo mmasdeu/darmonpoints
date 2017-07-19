ba42fdf590fa59c44702ecd47b889d418711a9a1
Marc Masdeu <marc.masdeu@gmail.com>
2017-07-19 10:39:16 -0400

    Installing sagemath before darmonpoints.

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 set -e
 
-$HOME/SageMath/sage -pip install --upgrade --no-index -v -i https://pypi.python.org/pypi .
+$HOME/SageMath/sage -pip install --user --upgrade -v -i https://pypi.python.org/pypi sagemath
+$HOME/SageMath/sage -pip install --upgrade --no-index -v .
 $HOME/SageMath/sage setup.py test
 (cd docs && $HOME/SageMath/sage -sh -c "make html")
 $HOME/SageMath/sage -pip uninstall .
