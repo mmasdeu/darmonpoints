b4c9951d614816bda8f1b7c4c8d1573b3c988824
Marc Masdeu <marc.masdeu@gmail.com>
2018-08-27 11:30:36 +0200

    Undone wget's changes.

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,7 +4,7 @@ SAGE_IMAGE=`python2 -c "import setup; print setup.get_all_version_names('${SAGE_
 cd $HOME
 if [ ! -x SageMath/sage ] ; then
     rm -f SageMath.tar.bz2
-    wget -nv ${SAGE_SERVER}${SAGE_IMAGE} -O SageMath.tar.bz2
+    wget ${SAGE_SERVER}${SAGE_IMAGE} -O SageMath.tar.bz2
     tar xf SageMath.tar.bz2
 fi
 MAKE="make -j4"
