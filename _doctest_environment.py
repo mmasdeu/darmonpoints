7be39d48d81f16ea646b244690417a8f851b2095
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-05-29 07:00:50 -0700

    tox.ini, _doctest_environment.py: New

---
+++ _doctest_environment.py
@@ -0,0 +1,6 @@
+# Toplevel for doctesting with passagemath
+
+from sage.all__sagemath_flint import *
+from sage.all__sagemath_pari import *
+from sage.all__sagemath_modules import *
+from sage.all__sagemath_symbolics import *
