1961ec47cf21d3059fb8d858d60f1a71e34e5819
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-05-29 07:46:49 -0700

    _doctest_environment.py: Add all__sagemath_schemes

--- _doctest_environment.py
+++ _doctest_environment.py
@@ -3,4 +3,5 @@
 from sage.all__sagemath_flint import *
 from sage.all__sagemath_pari import *
 from sage.all__sagemath_modules import *
+from sage.all__sagemath_schemes import *
 from sage.all__sagemath_symbolics import *
